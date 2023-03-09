bfd98384b640578506bf2b9e08ca6ae74e83ab42
andres9011 <54340539+andres9011@users.noreply.github.com>
2023-03-09 18:13:50 -0500

    mensaje

---
+++ src/perceptor/multicapa/Perceptor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perceptor.multicapa;
+
+/**
+ *
+ * @author Andres
+ */
+public class Perceptor 
+{
+   private double NEntradas[];
+   private double MSalidas[];
+   private double Ocultas[];
+   private double EjentrSal[][];
+   private double Potenciales[];
+   public double lambda1;
+   public double lambda2;  
+   double epsilon;
+   public double ECMs;
+   public double pECMs;
+   public double ECMv;
+   public double pECMv;
+
+}
